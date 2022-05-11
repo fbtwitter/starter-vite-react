757a20228fe14e79a6454edf4b93c9a3b0660e14
fbtwitter <rzaugusdi354@gmail.com>
2022-05-11 10:51:47 +0700

    fix base starter app

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './scss/style.scss'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<Router>
+			<Routes>
+				<Route path='/' element={<App />}>
+					<Route index element={<HomePage />} />
+				</Route>
+			</Routes>
+		</Router>
+	</React.StrictMode>
+)
