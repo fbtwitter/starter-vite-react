7ffc1b40036b7b8778bccd104b89f8ff51188e11
fbtwitter <rzaugusdi354@gmail.com>
2022-05-11 11:24:20 +0700

    add linter eslint stylelint prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom'
 
 function App() {
-	return (
-		<div className='App'>
-			<Outlet />
-		</div>
-	)
+  return (
+    <div className="App">
+      <Outlet />
+    </div>
+  )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<Router>
-			<Routes>
-				<Route path='/' element={<App />}>
-					<Route index element={<HomePage />} />
-				</Route>
-			</Routes>
-		</Router>
-	</React.StrictMode>
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>
 )
