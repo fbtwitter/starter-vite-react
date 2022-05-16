6c06a43313ad53d50fbaf96bd9299cc588228a7d
fbtwitter <rzaugusdi354@gmail.com>
2022-05-16 16:33:29 +0700

    update editor

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './scss/style.scss'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
+import Home from './pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<App />}>
-          <Route index element={<HomePage />} />
+          <Route index element={<Home />} />
         </Route>
       </Routes>
     </Router>
