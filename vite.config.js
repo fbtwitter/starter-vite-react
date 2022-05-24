64bcafdb85b74c78617a7d0531d521b86b3596f8
fbtwitter <rzaugusdi354@gmail.com>
2022-05-24 09:19:18 +0700

    add import svg feature

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const path = require('path');
+import svgr from 'vite-plugin-svgr'
+// eslint-disable-next-line no-undef
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
+      // eslint-disable-next-line no-undef
       '@': path.resolve(__dirname, './src'),
     },
   },
-  plugins: [react()]
+  plugins: [svgr(), react()],
 })
